0bb230b779f757f0e0660e4a846257dc0eed0c8d
Egemen Can Güler <egemencanguler@gmail.com>
2018-12-01 15:05:40 +0300

    prefab list

---
+++ PopTart/Assets/TeamArt/Scripts/BoardLoader.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Dalak;
+
+public class BoardLoader : MonoBehaviour
+{
+
+    public TextAsset boardText;
+    public PrefabList prefabList;
+    
+    
+
+    void Awake()
+    {
+        GenerateBoard(boardText.text);
+    }
+
+    void GenerateBoard(string text)
+    {
+        
+    }
+
+    void Generate(int id, Vector2 pos)
+    {
+        if (id == 0)
+        {
+            Debug.Log("Empty");
+            return;
+        }
+
+        GameObject prefab = prefabList.GetPrefab(id);
+        Instantiate(prefab).transform.position = pos;
+        
+    }
+}
---
+++ PopTart/Assets/TeamArt/Scripts/PrefabList.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "PrefabList",menuName = "PopTart/PrefabList")]
+public class PrefabList : ScriptableObject
+{
+    [SerializeField] GameObject[] prefabs;
+
+    public enum Type
+    {
+        OurDog = 1,
+        Wall = 2
+    }
+    
+    
+    public GameObject GetPrefab(int prefabIdx)
+    {
+        return prefabs[(int) prefabIdx];
+    }
+    
+    public GameObject GetPrefab(Type type)
+    {
+        return prefabs[(int) type];
+    }
+
+}
